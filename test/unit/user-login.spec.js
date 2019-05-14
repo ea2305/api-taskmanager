6d19207d6d3a7b15f05b043df7df631b33933ae8
Elihu Cruz <elihuacruz@gmail.com>
2019-05-14 00:08:19 -0500

    Login test integration, wrong parameters

--- test/unit/user-login.spec.js
+++ test/unit/user-login.spec.js
@@ -6,7 +6,10 @@ trait('Test/ApiClient')
 
 /**
  * In this test we will cover the following aspects
- * login request
+ * [ok] login request with bad email
+ * [] login request with bad password
+ * login successful request
+ * login request with limit to request allowed
  */
 test('[Login] Request access with a wrong email', async ({ client }) => {
   // Create test user
@@ -17,6 +20,27 @@ test('[Login] Request access with a wrong email', async ({ client }) => {
     .send({
       email: 'bad_email@example.com',
       password: 'fake_password'
+    })
+    .end()
+  // Check response status
+  response.assertStatus(401)
+  
+  // check response content
+  response.assertJSONSubset({ error: 'bad credentials' })
+
+  // delete test user
+  await user.delete()
+})
+
+test('[Login] Request access with a wrong password', async ({ client }) => {
+  // Create test user
+  const user = await Factory.model('App/Models/User').create()
+
+  // Send request to API with invalid email
+  const response = await client.post('/api/v1/auth/login')
+    .send({
+      email: user.email,
+      password: 'bad_password___'
     })
     .end()
   // Check response status
